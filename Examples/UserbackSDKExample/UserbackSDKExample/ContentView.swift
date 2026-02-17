357be0a96b9a18024b9c6bc588a5197e4dc801a9
Adrian Liu <aliu@userback.io>
2026-02-17 15:15:28 +1000

    Add example

---
+++ Examples/UserbackSDKExample/UserbackSDKExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserbackSDKExample
+//
+//  Created by Adrian Liu on 17/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Examples/UserbackSDKExample/UserbackSDKExample/UserbackSDKExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserbackSDKExampleApp.swift
+//  UserbackSDKExample
+//
+//  Created by Adrian Liu on 17/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct UserbackSDKExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
