208131edd66cec1b51f6ce05390c8d0d0682a3e1
Adrian Liu <aliu@userback.io>
2026-02-17 15:15:28 +1000

    Add userback sdk load function and example

---
+++ Examples/UserbackSDKExample/UserbackSDKExample/UserbackSDKExampleApp.swift
@@ -0,0 +1,30 @@
+//
+//  UserbackSDKExampleApp.swift
+//  UserbackSDKExample
+//
+//  Created by Adrian Liu on 17/2/2026.
+//
+
+import SwiftUI
+import UserbackSDK
+
+@main
+struct UserbackSDKExampleApp: App {
+    init() {
+        UserbackSDK.shared.start(
+            accessToken: "P-munRw6sN7ExmKIuAwNvumliFy",
+            userData: ["id": "example-user-id", "info": ["name": "Example User"]],
+            widgetCSS: "https://app.dev.userback.net/dist/widget_dev/widget.min.css",
+            surveyURL: "https://app.dev.userback.net/s",
+            requestURL: "https://api.dev.userback.net/",
+            trackURL: "https://events.dev.userback.net",
+            widgetJSURL: "https://app.dev.userback.net/dist/widget_dev/widget.min.js?34"
+        )
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
